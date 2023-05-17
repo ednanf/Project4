fd1aac33530e82da05f372abbbd67267a4e8ef5a
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-17 11:24:38 -0300

    Part 4 completed.

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -14,16 +14,17 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
 // MARK: - Properties
     
-	var webView: WKWebView! // Used to store the web view itself.
+	var webView: WKWebView!     // Used to store the web view itself.
+    var progressView: UIProgressView!       // Necessary to keep track of the loading progress.
     
     
 // MARK: - Setup
     
     // loadView should be loaded *before* viewDidLoad()
-    override func loadView() {
-        webView = WKWebView()               // First, we created an instance of WKWebView and assigned it to the property we declared.
-        webView.navigationDelegate = self   // Then we assigned the navigationDelegate property to self (the current view controller).
-        view = webView                      // Finally, we made the root view of the view controller to be webView.
+    override func loadView() {					// ATTENTION: to conform to navigationDelegate, it has to be added above as an implemented protocol in "class"!!!
+        webView = WKWebView()               	// First, we created an instance of WKWebView and assigned it to the property we declared.
+        webView.navigationDelegate = self   	// Then we assigned the navigationDelegate property to self (the current view controller).
+        view = webView                      	// Finally, we made the root view of the view controller to be webView.
     }
     
     override func viewDidLoad() {
@@ -32,19 +33,35 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		// Fix for the overlapping content with clock/battery/signal!!
 		webView.scrollView.contentInsetAdjustmentBehavior = .automatic
 		
-                                                                    // ATTENTION: we need to turn a string into a URL type to put in URLRequest!!!
-        let url = URL(string: "https://www.hackingwithswift.com")!  // First, we create a new data type called URL,
-        webView.load(URLRequest(url: url))                          // then we create a new URLRequest object from that URL and give it to web view to load.
-        webView.allowsBackForwardNavigationGestures = true          // This line allows the gesture of swiping to move backwards and forward.
+		// First page when starting the app									// ATTENTION: we need to turn a string into a URL type to put in URLRequest!!!
+        let url = URL(string: "https://www.hackingwithswift.com")!  	// First, we create a new data type called URL,
+        webView.load(URLRequest(url: url))                          	// then we create a new URLRequest object from that URL and give it to web view to load.
+        
+		// This allows the gesture of swiping to move backwards and forward.
+		webView.allowsBackForwardNavigationGestures = true
         
         // Navigation bar button to open the website list.
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        // Progress bar setup
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)       // Observer to inform the progress.
+        
+        progressView = UIProgressView(progressViewStyle: .default)      // Creates a new UIProgressView instance, and
+        progressView.sizeToFit()                                        // sets the bar's size.
 		
+		// Bottom bar configuration and buttons
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)     // Can't be tapped, so it doesn't need target and action.
+		let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))       // Calls the reload() method.
+        let progressBar = UIBarButtonItem(customView: progressView)     // Wraps the UIProgressView in a UIBarButtonItem to be inserted in the toolbar
+        
+		toolbarItems = [spacer, progressBar, spacer, refresh]        // Toolbar associated with the view controller.
+		navigationController?.isToolbarHidden = false       // Forces showing the toolbar.
 	}
 	
     
 // MARK: - Functions
     
+	// Action required for the rightBarButtonItem
     @objc func openTapped() {
         // Creates the action sheet to show the website list.
         let websiteList = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
@@ -53,7 +70,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         websiteList.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
         websiteList.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
         websiteList.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
-        websiteList.addAction(UIAlertAction(title: "Cancel", style: .cancel)) // Adds a dedicated cancel button.
+        websiteList.addAction(UIAlertAction(title: "Cancel", style: .cancel))       // Adds a dedicated cancel button.
         
         // This line is necessary to know where the pop over will be anchored on iPad.
         websiteList.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
@@ -73,7 +90,13 @@ class ViewController: UIViewController, WKNavigationDelegate {
 		title = webView.title
 	}
 	
+    // Observes the "estimatedProgress" value from WKWebview. Needed for the progress bar setup.
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)        // UIProgressView is a Float, so we need to convert estimatedProgress (which is a Double).
+        }
+    }
 	
 	
-	
+    
 }
